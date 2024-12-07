1b7cb1f993fc12529c04d309463171b73341adcb
Devendra Singh <113914000+Dev7083@users.noreply.github.com>
2024-12-07 22:31:29 +0530

    vercel deployment

--- codebot/settings.py
+++ codebot/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = "django-insecure-=z_nwrr2vd^v1p7y33sd#=d#4q-n%po&nyiy0^1t*n96&1r_m!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
